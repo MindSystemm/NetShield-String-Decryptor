ae30d6f6c68f83fa92a5377a6250a1b12c521d68
MindSystemm <mindlockreverser@gmail.com>
2017-07-19 15:39:04 +0200

    Initial Release
    .

---
+++ NetShield String Decryptor/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace NetShield_String_Decryptor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(52, 103);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(201, 192);
+            this.pictureBox1.TabIndex = 13;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(8, 74);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(291, 23);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Decrypt Strings";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(259, 9);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(42, 20);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.AllowDrop = true;
+            this.textBox1.Location = new System.Drawing.Point(8, 9);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(245, 20);
+            this.textBox1.TabIndex = 8;
+            this.textBox1.DragDrop += new System.Windows.Forms.DragEventHandler(this.TextBox1DragDrop);
+            this.textBox1.DragEnter += new System.Windows.Forms.DragEventHandler(this.TextBox1DragEnter);
+            // 
+            // textBox2
+            // 
+            this.textBox2.AllowDrop = true;
+            this.textBox2.Location = new System.Drawing.Point(56, 41);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(245, 20);
+            this.textBox2.TabIndex = 14;
+            this.textBox2.DragDrop += new System.Windows.Forms.DragEventHandler(this.TextBox2DragDrop);
+            this.textBox2.DragEnter += new System.Windows.Forms.DragEventHandler(this.TextBox2DragEnter);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(5, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "List : ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(309, 307);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "NetShield String Decryptor";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ NetShield String Decryptor/Form1.cs
@@ -0,0 +1,186 @@
+﻿using dnlib.DotNet;
+using dnlib.DotNet.Emit;
+using dnlib.DotNet.Writer;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NetShield_String_Decryptor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void TextBox1DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.Copy;
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
+        }
+        private void TextBox2DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.Copy;
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
+        }
+        private void TextBox2DragDrop(object sender, DragEventArgs e)
+        {
+            try
+            {
+                Array array = (Array)e.Data.GetData(DataFormats.FileDrop);
+                if (array != null)
+                {
+                    string text = array.GetValue(0).ToString();
+                    int num = text.LastIndexOf(".", StringComparison.Ordinal);
+                    if (num != -1)
+                    {
+                        string text2 = text.Substring(num);
+                        text2 = text2.ToLower();
+                        if (text2 == ".txt")
+                        {
+                            Activate();
+                            textBox2.Text = text;
+                            int num2 = text.LastIndexOf("\\", StringComparison.Ordinal);
+                            if (num2 != -1)
+                            {
+                                DirectoryName = text.Remove(num2, text.Length - num2);
+                            }
+                            if (DirectoryName.Length == 2)
+                            {
+                                DirectoryName += "\\";
+                            }
+                        }
+                    }
+                }
+            }
+            catch
+            {
+            }
+        }
+        private void TextBox1DragDrop(object sender, DragEventArgs e)
+        {
+            try
+            {
+                Array array = (Array)e.Data.GetData(DataFormats.FileDrop);
+                if (array != null)
+                {
+                    string text = array.GetValue(0).ToString();
+                    int num = text.LastIndexOf(".", StringComparison.Ordinal);
+                    if (num != -1)
+                    {
+                        string text2 = text.Substring(num);
+                        text2 = text2.ToLower();
+                        if (text2 == ".exe" || text2 == ".dll")
+                        {
+                            Activate();
+                            textBox1.Text = text;
+                            int num2 = text.LastIndexOf("\\", StringComparison.Ordinal);
+                            if (num2 != -1)
+                            {
+                                DirectoryName = text.Remove(num2, text.Length - num2);
+                            }
+                            if (DirectoryName.Length == 2)
+                            {
+                                DirectoryName += "\\";
+                            }
+                        }
+                    }
+                }
+            }
+            catch
+            {
+            }
+        }
+        string DirectoryName = "";
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Title = "Browse for target assembly";
+            openFileDialog.InitialDirectory = "c:\\";
+            if (DirectoryName != "")
+            {
+                openFileDialog.InitialDirectory = this.DirectoryName;
+            }
+            openFileDialog.Filter = "All files (*.exe,*.dll)|*.exe;*.dll";
+            openFileDialog.FilterIndex = 2;
+            openFileDialog.RestoreDirectory = true;
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string fileName = openFileDialog.FileName;
+                textBox1.Text = fileName;
+                int num = fileName.LastIndexOf("\\", StringComparison.Ordinal);
+                if (num != -1)
+                {
+                    DirectoryName = fileName.Remove(num, fileName.Length - num);
+                }
+                if (DirectoryName.Length == 2)
+                {
+                    DirectoryName += "\\";
+                }
+            }
+        }
+        public void StringDecryptor(ModuleDefMD module)
+        {
+            foreach (TypeDef type in module.Types)
+            {
+               foreach(MethodDef method in type.Methods)
+                {
+                    if(method.HasBody)
+                    {
+                        if(method.Body.HasInstructions)
+                        {
+                            for (int i = 0; i < method.Body.Instructions.Count; i++)
+                            {
+                                if (method.Body.Instructions[i].IsLdcI4() && method.Body.Instructions[i + 1].OpCode == OpCodes.Call && method.Body.Instructions[i + 1].Operand.ToString().Contains("System.String"))
+                                {
+                                    int index = (int)method.Body.Instructions[i].GetLdcI4Value();
+                                    string[] data = File.ReadAllLines(textBox2.Text);
+                                    string result = data[index];
+                                    string corrected = result.Replace("\"", "");
+                                    method.Body.Instructions[i] = Instruction.Create(OpCodes.Ldstr, corrected);
+                                    method.Body.Instructions[i + 1].OpCode = OpCodes.Nop;
+                                }
+                            }
+                        }
+                    }
+                }
+                    
+            }
+
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ModuleDefMD module = ModuleDefMD.Load(textBox1.Text);
+            StringDecryptor(module);
+            string text2 = Path.GetDirectoryName(textBox1.Text);
+            if (!text2.EndsWith("\\"))
+            {
+                text2 += "\\";
+            }
+            string path = text2 + Path.GetFileNameWithoutExtension(textBox1.Text) + "_Decrypter" +
+                          Path.GetExtension(textBox1.Text);
+            var opts = new ModuleWriterOptions(module);
+            opts.Logger = DummyLogger.NoThrowInstance;
+            module.Write(path, opts);
+        }
+    }
+}
